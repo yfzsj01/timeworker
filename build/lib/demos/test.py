7c03517b50508cb77ce643a79fe7d030a35f9d77
Daniel Zzzsj <zsj@DanielZzzsjMacAir.local>
2022-12-12 16:25:39 +0800

    v1.2.2

---
+++ build/lib/demos/test.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/12/8 20:01
+# @Author  : vicissitude
+import timeworker
+
+time_str = '上个月'
+print(timeworker.parse(time_str))
---
+++ timeworker/data/__init__.py
@@ -0,0 +1,2 @@
+from timeworker.data import date_translation_data
+from .languages_info import language_order, language_locale_dict
